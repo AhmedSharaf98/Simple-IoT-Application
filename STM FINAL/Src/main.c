33eca2be3d424d9f443fd124ddfe9b05da7df5aa
Ahmed Sharaf <ahmedsharaf@aucegypt.edu>
2020-05-27 08:00:18 +0200

    Support RTC

--- STM FINAL/Src/main.c
+++ STM FINAL/Src/main.c
@@ -1,96 +1,88 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART1_UART_Init(void);
-/* USER CODE BEGIN PFP */
+static void MX_I2C1_Init(void);
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-	
 uint8_t msg[13] =  "Hello World\n\r";
 char buffer [16];
 short bufferLocation = -1;
 uint8_t rec[1];
 int process = 0;
 
-
-
 const char TOGGLE_LED[16] = "ToggleLed";
 const char GET_TIME[16] = "GetTime";
 const char SET_ALARM[16] = "SetAlarm";
 
+/* USER CODE BEGIN 0 */
+uint8_t secbuffer [2], minbuffer [2], hourbuffer [2];
+uint8_t a1secbuffer [2], a1minbuffer [2], a1hourbuffer [2], contbuffer [2], statusbuffer [2];
+uint8_t out[] = {0,0,':',0,0,':',0,0,'\r','\n'};
+
+uint8_t hexToAscii(uint8_t n)//4-bit hex value converted to an ascii character
+{
+ if (n>=0 && n<=9) n = n + '0';
+ else n = n - 10 + 'A';
+ return n;
+}
+
+void setClock(){ 
+	 // seconds
+	 secbuffer[0] = 0x00; //register address
+	 secbuffer[1] = 0x00; //data to put in register --> 0 sec
+	 HAL_I2C_Master_Transmit(&hi2c1, 0xD0, secbuffer, 2, 10);
+	 // minutes
+	 minbuffer[0] = 0x01; //register address
+	 minbuffer[1] = 0x30; //data to put in register --> 30 min
+	 HAL_I2C_Master_Transmit(&hi2c1, 0xD0, minbuffer, 2, 10);
+	 // hours
+	 hourbuffer[0] = 0x02; //register address
+	 hourbuffer[1] = 0x11; //data to put in register 00010001 --> 11 am
+	 HAL_I2C_Master_Transmit(&hi2c1, 0xD0, hourbuffer, 2, 10);
+}
+
+void getTime(){
+	HAL_I2C_Master_Transmit(&hi2c1, 0xD0, secbuffer, 1, 10);
+	 //read data of register 00h to secbuffer[1]
+	 HAL_I2C_Master_Receive(&hi2c1, 0xD1, secbuffer+1, 1, 10);
+	 //prepare UART output
+	 out[6] = hexToAscii(secbuffer[1] >> 4 );
+	 out[7] = hexToAscii(secbuffer[1] & 0x0F );
+	 HAL_I2C_Master_Transmit(&hi2c1, 0xD0, minbuffer, 1, 10);
+	 HAL_I2C_Master_Receive(&hi2c1, 0xD1, minbuffer+1, 1, 10);
+	 out[3] = hexToAscii(minbuffer[1] >> 4 );
+	 out[4] = hexToAscii(minbuffer[1] & 0x0F );
+	 HAL_I2C_Master_Transmit(&hi2c1, 0xD0, hourbuffer, 1, 10);
+	 HAL_I2C_Master_Receive(&hi2c1, 0xD1, hourbuffer+1, 1, 10);
+	 out[0] = hexToAscii(hourbuffer[1] >> 4);
+	 out[1] = hexToAscii(hourbuffer[1] & 0x0F);
+}
+
 void handleRequest(){
 	HAL_UART_Transmit(&huart2, "Handling\n\r", 10,500);
 		if(strcmp(buffer, TOGGLE_LED) == 0){
-			HAL_UART_Transmit(&huart2, "Toggling", 8,500);
+			HAL_UART_Transmit(&huart2, "Togglled\n\r", 10,500);
 			HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
 		}
-		
-		HAL_UART_Transmit(&huart2, buffer, 16,500);
+		if(strcmp(buffer, GET_TIME) == 0){
+			HAL_UART_Transmit(&huart2, "Getting Time\n\r", 14,500);
+			getTime();
+			HAL_UART_Transmit(&huart2,out, sizeof(out), 10);
+		} else {
+			HAL_UART_Transmit(&huart2, buffer, 16,500);
+		}
 		bufferLocation = 0;
 		memset(buffer, 0, 16);
 		process = 0;
 }
+
+
 int main(void)
 {
   HAL_Init();
@@ -98,8 +90,17 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_USART1_UART_Init();
- 
-	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
+  MX_I2C1_Init();
+	
+	if (HAL_I2C_IsDeviceReady(&hi2c1, 0xD0, 10, HAL_MAX_DELAY) == HAL_OK)
+	 {
+			for (int i = 1; i<=10;i++){
+				 HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+				 HAL_Delay(100);
+			}
+	 }
+	setClock();
+  __HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_TC);
 	__HAL_UART_ENABLE_IT(&huart2, UART_IT_RXNE);
 	__HAL_UART_ENABLE_IT(&huart2, UART_IT_TC);
@@ -150,9 +151,11 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2
+                              |RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -166,20 +169,46 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00000E14;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter 
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter 
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
   */
 static void MX_USART1_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -194,10 +223,7 @@ static void MX_USART1_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
+  
 }
 
 /**
@@ -207,14 +233,6 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_USART2_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 9600;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -229,10 +247,6 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
@@ -260,22 +274,6 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-
-  /* USER CODE END Error_Handler_Debug */
-}
-
 /**
   * @brief This function handles USART1 global interrupt.
   */
@@ -300,6 +298,23 @@ void USART2_IRQHandler(void)
   HAL_UART_IRQHandler(&huart2);
 	HAL_UART_Receive_IT(&huart2, temp, 1);
 }
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
 #ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
